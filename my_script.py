dc64c1448c97e43657359200f0d128f8c744b450
Tahseen Ullah Ihsaan <tahseenislamian900@gmail.com>
2024-11-18 12:21:04 +0500

    this commit contian to coorect  the code

--- my_script.py
+++ my_script.py
@@ -1,4 +1,4 @@
 print("HELLO TAHSEENULLAH IHSAN") 
 print("i am tahseenullah ihsan  i a devops engineer at xgird ") 
-prin("i am tahseenullah ihsan  i a devops engineer at xgird ") 
+print("i am tahseenullah ihsan  i a devops engineer at xgird ") 
 
