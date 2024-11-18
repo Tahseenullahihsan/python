f89a37051f1412cddfcf7046177d6cfe884a4402
Tahseen Ullah Ihsaan <tahseenislamian900@gmail.com>
2024-11-18 14:41:48 +0500

    this commit contain the index.html file

--- my_script.py
+++ my_script.py
@@ -1,4 +1,5 @@
 print("HELLO TAHSEENULLAH IHSAN") 
 print("i am tahseenullah ihsan  i a devops engineer at xgird ") 
-print("i am tahseenullah ihsan  i a devops engineer at xgird ") 
+print("i am tahseenullah ihsan  i a devops engineer at xgird ")
+print("i am tahseenullah ihsan  i a devops engineer at xgird ")  
 
